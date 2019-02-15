fa2568187a85c0eaf7f43d389a0db175a26a4c35
Russel Simmons <russ@rsimmons.org>
2019-02-15 14:42:56 -0800

    Convert PriorityQueue to TS

--- runtime/src/PriorityQueue.ts
+++ runtime/src/PriorityQueue.ts
@@ -1,28 +1,35 @@
 import FastPriorityQueue from 'fastpriorityqueue';
 
+interface Element {
+  readonly priority: string;
+  readonly data: any;
+}
+
 export default class PriorityQueue {
+  fpq: any
+
   constructor() {
-    this.fpq = new FastPriorityQueue((a, b) => (a.priority < b.priority));
+    this.fpq = new FastPriorityQueue((a: Element, b: Element) => (a.priority < b.priority));
   }
 
-  isEmpty() {
+  isEmpty(): boolean {
     return this.fpq.isEmpty();
   }
 
   // element is expected to have "priority" property that is a string
-  insert(priority, data) {
+  insert(priority: string, data: any): void {
     this.fpq.add({priority, data});
   }
 
-  pop() {
+  pop(): any {
     return this.fpq.poll().data;
   }
 
-  peek() {
+  peek(): any {
     return this.fpq.peek().data;
   }
 
-  clear() {
+  clear(): void {
     while (!this.fpq.isEmpty()) {
       this.fpq.poll();
     }
